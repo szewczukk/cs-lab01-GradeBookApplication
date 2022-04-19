be36f63bf89a2d29a6dc61691fa02db3441b7f85
szewczukk <dmitro.szewczuk@gmail.com>
2022-04-19 19:42:16 +0200

    Create RankedGradeBook class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base (name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            var studentCount = Students.Count;
+
+            if (studentCount < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var betterStudents = 0;
+            foreach (var student in Students)
+            {
+                if (student.AverageGrade > averageGrade)
+                {
+                    betterStudents++;
+                }
+            }
+            
+            var betterStudentsPercentage = betterStudents / studentCount;
+            if(betterStudentsPercentage <= 0.2)
+            {
+                return 'A';
+            }
+            if(betterStudentsPercentage > 0.2 && betterStudentsPercentage <= 0.4)
+            {
+                return 'B';
+            }
+            if(betterStudentsPercentage > 0.4 && betterStudentsPercentage <= 0.6)
+            {
+                return 'C';
+            }
+            if (betterStudentsPercentage > 0.6 && betterStudentsPercentage <= 0.8)
+            {
+                return 'D';
+            }
+            return 'F';
+        }
+    }
+}
